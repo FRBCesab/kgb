f54819b1416bf11877a5084c2121531049fd0181
camillecoux <camille.coux@orange.fr>
2023-03-27 18:03:36 +0200

    added the dafnee refs

---
+++ analyses/get_dafnee_data.R
@@ -0,0 +1,8 @@
+
+# get the refs from Cesab, NCEAS and sDiv already compiled for DAFNEE
+utils::download.file("https://raw.githubusercontent.com/FRBCesab/dafnee/main/data/derived-data/all-publications.txt", 
+                     "data/raw-data/dafnee_refs.txt", 
+                     mode = "wb" )
+
+
+
